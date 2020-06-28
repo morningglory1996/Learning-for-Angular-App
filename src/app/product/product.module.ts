7b7c0e8995e9a7788637621c79097d62859d1f01
morningglory1996 <acer080428@outlook.jp>
2020-06-28 20:25:18 +0900

    add product moudle

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { from } from 'rxjs';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+  { 
+    path: 'products', component: ProductComponent ,
+    children: [
+      { path: '', component: ProductListComponent},
+      { path: ':productId', component: ProductDetailComponent}
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductComponent
+  ],
+  imports: [
+    [RouterModule.forChild(routes)],
+    CommonModule
+  ]
+})
+export class ProductModule { }
