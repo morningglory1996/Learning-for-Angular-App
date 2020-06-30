637c719e6952e959a4d044bc201a4fea3585a369
morningglory1996 <acer080428@outlook.jp>
2020-06-30 18:36:39 +0900

    setup for Heroku

--- server/index.js
+++ server/index.js
@@ -3,40 +3,40 @@ const mongoose = require('mongoose');
 const app = express();
 const productRoutes = require('./routes/products');
 
-const config = require('./config/dev');
+const config = require('./config/index');
 const SampleDb = require('./sample-db');
 const { EOVERFLOW } = require('constants');
 
-const PORT = process.env.PORT || 3001;
+
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  console.log('MongoDB connection successful');
-  const sampleDb = new SampleDb();
-  sampleDb.initDb();
+  if(process.env.NODE_ENV !== 'production') {
+    console.log('MongoDB connection successful');
+    const sampleDb = new SampleDb();
+    // sampleDb.initDb();
+  }
 }).catch((error) => {
   console.log(error);
 });
 
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-// db.once('open', () => console.log('MongoDB connection successful'))
+
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'resavation-app');
+  app.use(express.static(appPath));
+  app.get('*', (req, res) => {
+    res.send(path.resolve(appPath, 'index.html'));
+  });
+}
+
 
 app.use('/api/v1/products', productRoutes);
-
 app.listen(PORT, () => {
   console.log('server listening on ' + PORT);
 });
-
-
-// mongoose.connect('mongodb+srv://test:test@cluster0-0viyl.mongodb.net/<dbname>?retryWrites=true&w=majority',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }
-// )
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-// db.once('open', () => console.log('MongoDB connection successful'))
