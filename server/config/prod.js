2ed37dde1f192866c372f71d4cd64b5e2c821836
morningglory1996 <acer080428@outlook.jp>
2020-06-30 21:38:24 +0900

    test

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = {
-  // DB_URI: process.env.DB_URI
-  DB_URI: 'mongodb+srv://dbReserve:testtest1@cluster0.i3neg.mongodb.net/test?retryWrites=true&w=majority'
+  DB_URI: process.env.DB_URI
 }
--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const productRoutes = require('./routes/products');
 const path = require('path');
 
-// const config = require('./config');
-const config = require('./config/prod');
+const config = require('./config');
 
 const SampleDb = require('./sample-db');
 const { EOVERFLOW } = require('constants');
@@ -15,11 +14,11 @@ mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  // if(process.env.NODE_ENV !== 'production') {
+  if(process.env.NODE_ENV !== 'production') {
   console.log('MongoDB connection successful');
   const sampleDb = new SampleDb();
   // sampleDb.initDb();
-  // }
+  }
 }).catch((error) => {
   console.log(error);
 });
@@ -28,13 +27,13 @@ mongoose.connect(config.DB_URI, {
 const app = express();
 app.use('/api/v1/products', productRoutes);
 
-// if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   const appPath = path.join(__dirname, '..', 'dist', 'resavation-app');
   app.use(express.static(appPath));
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(appPath, 'index.html'));
   });
-// }
+}
 
 const PORT = process.env.PORT || 3001;
 
