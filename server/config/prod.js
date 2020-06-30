dd6a41e1912cda07f727ff4e60b84f418fe1ea6c
morningglory1996 <acer080428@outlook.jp>
2020-06-30 21:31:44 +0900

    test

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  // DB_URI: process.env.DB_URI
+  DB_URI: 'mongodb+srv://dbReserve:testtest1@cluster0.i3neg.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-const productRoutes = require('./routes/products');
 
-const config = require('./config/dev');
+const productRoutes = require('./routes/products');
+const path = require('path');
+
+// const config = require('./config');
+const config = require('./config/prod');
+
 const SampleDb = require('./sample-db');
 const { EOVERFLOW } = require('constants');
 
-const PORT = process.env.PORT || 3001;
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
+  // if(process.env.NODE_ENV !== 'production') {
   console.log('MongoDB connection successful');
   const sampleDb = new SampleDb();
-  sampleDb.initDb();
+  // sampleDb.initDb();
+  // }
 }).catch((error) => {
   console.log(error);
 });
 
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-// db.once('open', () => console.log('MongoDB connection successful'))
 
+const app = express();
 app.use('/api/v1/products', productRoutes);
+
+// if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'resavation-app');
+  app.use(express.static(appPath));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+// }
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log('server listening on ' + PORT);
 });
-
-
-// mongoose.connect('mongodb+srv://test:test@cluster0-0viyl.mongodb.net/<dbname>?retryWrites=true&w=majority',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }
-// )
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-// db.once('open', () => console.log('MongoDB connection successful'))
