29d6f3e7a139a7b2f6cdab00d3a028ffdb07c8dd
morningglory1996 <acer080428@outlook.jp>
2020-07-01 18:19:12 +0900

    add login&register frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
@@ -10,7 +12,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    ProductModule
+    ProductModule,
+    AuthModule
   ],
   exports: [RouterModule]
 })
